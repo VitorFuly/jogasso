efe9f4179b1f483150ed9ffafb7969f1be95049c
VitorFuly <40274380+VitorFuly@users.noreply.github.com>
2018-06-16 03:27:05 -0300

    Update main.h

--- main.h
+++ main.h
@@ -2,10 +2,16 @@
 #include "SDL.h"
 #include "SDL_ttf.h"
 #include "SDL_image.h"
+#include "SDL_mixer.h"
 
 #define STATUS_STATE_VIDAS 0
 #define STATUS_STATE_GAME 1	
 #define STATUS_STATE_GAMEOVER 2
+
+typedef struct {
+	float logox, logoy;
+}
+Logo;
 
 typedef struct {
 	float x, y;
@@ -31,19 +37,39 @@ typedef struct {
 	Man man;
 	//enfeite
 	Piso piso[100];
-
+	Logo logoi;
 
 	//imagens
+	SDL_Texture *logomenu;
+	SDL_Texture *logo;
 	SDL_Texture *paisa;
 	SDL_Texture *Boneco[2];
 	SDL_Texture *Chao;
 	SDL_Texture *Star;
 	SDL_Texture *label;
+	SDL_Texture *conti;
+	SDL_Texture *novo;
+	SDL_Texture *config;
+	SDL_Texture *sair;
 	int labelw, labelh;
+	int contiw, contih;
+	int sairw, sairh;
+	int configw, configh;
+	int novow, novoh;
+	int logow, logoh;
+	// som
+	
+	
+
+
+
 
 
 	//Font
 	TTF_Font *fonte;
+	TTF_Font *fonte2;
+	TTF_Font *fonte3;
+	TTF_Font *fonte4;
 	// tempo
 	int tempo;
 	int statusState;
