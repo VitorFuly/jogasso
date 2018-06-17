64c6ad223e8c44715664ddbd943281f62d2f2f1d
VitorFuly <40274380+VitorFuly@users.noreply.github.com>
2018-06-17 19:04:01 -0300

    Update main.h

--- main.h
+++ main.h
@@ -31,6 +31,7 @@ typedef struct {
 }
 Piso;
 typedef struct {
+	float scrollx;
 	//fundo
 	Paisagem paisagem;
 	//players
@@ -39,29 +40,44 @@ typedef struct {
 	Piso piso[100];
 	Logo logoi;
 
-	//imagens
+	//IMAGENS INGAME
 	SDL_Texture *logomenu;
 	SDL_Texture *logo;
 	SDL_Texture *paisa;
 	SDL_Texture *Boneco[2];
 	SDL_Texture *Chao;
 	SDL_Texture *Star;
+
+	//MENU INICIAR
 	SDL_Texture *label;
 	SDL_Texture *conti;
+	SDL_Texture *conti1;
 	SDL_Texture *novo;
+	SDL_Texture *novo1;
 	SDL_Texture *config;
+	SDL_Texture *config1;
 	SDL_Texture *sair;
+	SDL_Texture *sair1;
 	int labelw, labelh;
 	int contiw, contih;
 	int sairw, sairh;
 	int configw, configh;
 	int novow, novoh;
 	int logow, logoh;
+
+
+	// MENU CONFIG
+	
+
+	// INFO DO MOUSE
+	int mousex, mousey;
+	int sobconti;
+	int sobnovo;
+	int sobconfig;
+	int sobsair;
 	// som
 	
-	
-
-
+	Mix_Chunk *menusom;
 
 
 
@@ -70,9 +86,11 @@ typedef struct {
 	TTF_Font *fonte2;
 	TTF_Font *fonte3;
 	TTF_Font *fonte4;
+
 	// tempo
 	int tempo;
 	int statusState;
+
 	//renderer
 	SDL_Renderer *renderer;
 }
