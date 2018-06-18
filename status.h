69407bcc57849936f1d50e7acdb34f7e3851c6e3
VitorFuly <40274380+VitorFuly@users.noreply.github.com>
2018-06-18 01:52:23 -0300

    Update status.h

--- status.h
+++ status.h
@@ -1,7 +1,11 @@
 #pragma once
 
-void init_status_vidas(GameState *gameState);
+void init_status_menu(GameState *gameState);
 
-void draw_status_vidas(GameState *gameState);
+void draw_status_menu(GameState *gameState);
 
-void shutdown_status_vidas(GameState *gameState);
+void shutdown_status_menu(GameState *gameState);
+
+void init_status_cred(GameState *gameState);
+void draw_status_cred(GameState *gameState);
+void shutdown_status_cred(GameState *gameState);
