5e0ab187549d7af743488d4b4bc65ecc9829c496
VitorFuly <40274380+VitorFuly@users.noreply.github.com>
2018-07-07 22:19:47 -0300

    Update status.h

--- status.h
+++ status.h
@@ -9,3 +9,8 @@ void shutdown_status_menu(GameState *gameState);
 void init_status_cred(GameState *gameState);
 void draw_status_cred(GameState *gameState);
 void shutdown_status_cred(GameState *gameState);
+
+
+void init_status_over(GameState *game);
+void draw_status_over(GameState *game);
+void shutdown_status_over(GameState *game);
