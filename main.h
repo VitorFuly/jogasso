c1d834ba6bc37170fccc270edf083cf42827fbc0
VitorFuly <40274380+VitorFuly@users.noreply.github.com>
2018-07-12 07:58:48 -0300

    Update main.h

--- main.h
+++ main.h
@@ -5,6 +5,7 @@
 #include "SDL_image.h"
 #include "SDL_mixer.h"
 
+
 #define _CRT_SECURE_NO_WARNINGS
 #define STATUS_STATE_MENU 0
 #define STATUS_STATE_GAME 1	
@@ -12,6 +13,7 @@
 #define STATUS_STATE_CONFIG 3
 #define STATUS_STATE_CREDITS 4
 #define STATUS_STATE_CONTROLES 5
+#define GAME_STATUS_FIM 6
 #define MAX_TIROS 10
 
 typedef struct {
@@ -25,14 +27,18 @@ typedef struct {
 } Enemy;
 
 typedef struct {
-	float xt, yt,dxt,fimtiro;
-}Tiro;
-
+	float x, y, w, h, vivo, vidas, baseX, baseY, mode, dx;
+	int  aocontrario, phase, mov;
+} Morcego;
 
 typedef struct {
 	float x, y, h, w,baseX,baseY;
 	int starviva,visivel,mode;
 }Estrela;
+
+typedef struct {
+	float x, y;
+} Corda ;
 
 
 typedef struct {
@@ -64,33 +70,48 @@ typedef struct {
 }HUD;
 
 
+FILE *f;
+
 typedef struct {
 	float scrollx;
+
+	Corda corda1;
 	//fundo
 	Paisagem paisagem;
 	//players
 	Man man;
 	//enfeite
-	Enemy inimigo[10];
+	Enemy inimigo[20];
 	Vida heart[3];
 	Piso piso[100];
 	Plat plataform[100];
 	Logo logoi;
 	Estrela Star[100];
+	Morcego morcego1[50];
+
+	Piso piso2[100];
+	Piso plataforma2[100];
+	char pontuacao[100];
 	
-
-	Piso piso1[100];
-
-
-	Tiro *shot[MAX_TIROS];
 
 	SDL_Texture *tirotext;
 
 
-
+	int n, n1, n2, n3, aux;
 
 	//IMAGENS INGAME mapa 1
-	
+	SDL_Texture *sairgoi3;
+	SDL_Texture *voltarfim;
+	SDL_Texture *pontof1i2;
+	SDL_Texture *sairgoi4;
+	SDL_Texture *voltargoi2;
+	SDL_Texture *fundofim;
+
+
+	SDL_Texture *pontof1i;
+	SDL_Texture *pontof2i;
+	SDL_Texture *corda;
+	SDL_Texture *placacorda;
 	SDL_Texture *hudi;
 	SDL_Texture *vidasti;
 	SDL_Texture *vidasi;
@@ -100,20 +121,21 @@ typedef struct {
 	SDL_Texture *logomenu;
 	SDL_Texture *logo;
 	SDL_Texture *paisa;
-	SDL_Texture *Boneco[4];
+	SDL_Texture *paisa2;
+	SDL_Texture *Boneco[3];
 	SDL_Texture *Chao;
 	SDL_Texture *star;
 	SDL_Texture *Plataforma;
 	SDL_Texture *fundocontrole;
 	SDL_Texture *estrela;
 	SDL_Texture *scorei;
-
+	
 
 	// ingame mapa 2
-	SDL_Texture *Chao2;
-
-
-
+	SDL_Texture *chaomapa2;
+	SDL_Texture *plataformamapa2;
+	SDL_Texture *fundomapa2;
+	SDL_Texture *morcego[2];
 	//MENU INICIAR
 	SDL_Texture *label;
 	SDL_Texture *novo;
@@ -125,8 +147,9 @@ typedef struct {
 	SDL_Texture *sair1;
 	SDL_Texture *cred;
 	SDL_Texture *cred1;
-	
-
+	SDL_Texture *rankingi;
+	SDL_Texture *rankingi1;
+	int fase2;
 	
 	int iden;
 	int labelw, labelh;
@@ -137,8 +160,10 @@ typedef struct {
 	int credw, credh;
 	int cred1w, cred1h;
 	int tempocred;
-	int encontrado;
-
+	
+	int cordachao;
+	int sobvoltarfim;
+	int sobsairfim;
 
 	// MENU CREDITOS
 	SDL_Texture *creditosi;
@@ -153,20 +178,28 @@ typedef struct {
 	SDL_Texture *voltarmi1;
 	SDL_Texture *devsi;
 	SDL_Texture *devsi1;
+	SDL_Texture *sairconfig;
+	SDL_Texture *voltarconfig;
+	SDL_Texture *configfundo;
+	SDL_Texture *tick;
+	SDL_Texture *voltarconfig1;
+	SDL_Texture *sairconfig1;
 
-
-
+	int sobtick;
+	int pont;
+	int sobsairm, sobvoltarm;
 	int creditosw, creditosh;
 	int vitorw, vitorh;
 	int ricardow, ricardoh;
 	int guilhermew, guilhermeh;
 	int voltarmenuw, voltarmenuh;
 	int devsw, devsh;
+	int recordeh, recordew;
 
 
-
-
+	SDL_Texture *audioi;
 	// menu gameover
+	SDL_Texture *textoi;
 	SDL_Texture *voltargoi;
 	SDL_Texture *sairgoi;
 	SDL_Texture *fundogo;
@@ -174,8 +207,14 @@ typedef struct {
 	SDL_Texture *sairgoi1;
 	int sobvoltargo;
 	int sobsairgo;
+	
+	 char text[10];
+	
+	Sint32 cursor;
+	 Sint32 selection_len;
 
 
+	 Mix_Music *musicafundo;
 
 	// INFO DO MOUSE
 	int mousex, mousey;
@@ -186,7 +225,7 @@ typedef struct {
 	int sobconfig;
 	int sobsair;
 	int sobcredi;
-
+	int sobranking;
 	//INFO DO MOUSE NOS CREDITOS
 
 	int sobvoltar;
@@ -194,7 +233,7 @@ typedef struct {
 	
 	Mix_Chunk *menusom;
 
-
+	Mix_Chunk *hit;
 
 	//Font
 	TTF_Font *fonte;
